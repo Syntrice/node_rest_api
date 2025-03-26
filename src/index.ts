1268c38e547acdf7db903b2f2fd6cc717084dbc6
Syntrice <syntrice@protonmail.com>
2025-03-26 12:59:46 +0000

    feat: Setup Express
    Add a basic express server setup with some essential middleware

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello Typescript")
+import express from "express"
+import http from "http"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import compression from "compression"
+import cors from "cors"
+
+// Setup the application. Works very much like asp.net
+
+const app = express()
+
+// Setup CORS
+app.use(cors({
+    credentials: true,
+}))
+
+// Configure dependencies
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app)
+
+// Listen for requests on port 8080
+server.listen(8080, () => {
+    console.log("Server running on port http://localhost:8080/")
+})

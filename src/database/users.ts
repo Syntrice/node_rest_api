27bd28f2b6c9a08c7d855889998c0e6d7e90537d
Syntrice <syntrice@protonmail.com>
2025-03-26 13:35:33 +0000

    feat: Implement user model

--- src/database/users.ts
+++ src/database/users.ts
@@ -9,3 +9,38 @@ const UserSchema = new mongoose.Schema({
         sessionToken: {type: String, select: false},
     },
 });
+
+// Create a model for the user
+export const UserModel = mongoose.model("User", UserSchema);
+
+// Simple functions for interacting with the database
+
+export const getUsers = () => {
+    return UserModel.find();
+}
+
+export const getUserByEmail = (email: string) => {
+    return UserModel.findOne({email: email});
+}
+
+export const getUserBySessionToken = (sessionToken: string) => {
+    return UserModel.findOne({"authentication.sessionToken": sessionToken});
+}
+
+export const getUserById = (id: string) => {
+    return UserModel.findById(id);
+}
+
+export const createUser = (values: Record<string, any>) => {
+    return new UserModel(values).save().then((user) => {
+        return user.toObject();
+    });
+}
+
+export const deleteUserById = (id: string) => {
+    return UserModel.findOneAndDelete({_id: id});
+}
+
+export const updateUserById = (id: string, values: Record<string, any>) => {
+    return UserModel.findOneAndUpdate({_id: id}, values);
+}

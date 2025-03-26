82a9f35d38fd992eb97693678e62e96e354babb4
Syntrice <syntrice@protonmail.com>
2025-03-26 13:26:07 +0000

    feat: Setup Mongoose
    Setup Mongoose for Mongodb connection. You will need to provide a
    connections string via an environment variable DB_CONNECTION_STRING.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,39 @@
-import express from "express"
-import http from "http"
-import bodyParser from "body-parser"
-import cookieParser from "cookie-parser"
-import compression from "compression"
-import cors from "cors"
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose, { mongo } from "mongoose";
 
 // Setup the application. Works very much like asp.net
 
-const app = express()
+const app = express();
 
 // Setup CORS
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-}))
+  })
+);
 
 // Configure dependencies
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 // Listen for requests on port 8080
 server.listen(8080, () => {
-    console.log("Server running on port http://localhost:8080/")
-})
+  console.log("Server running on port http://localhost:8080/");
+});
+
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+
+console.log(`DB_CONNECTION_STRING: ${DB_CONNECTION_STRING}`);
+
+// Construct a promise for mongoose to connect to the database
+mongoose.Promise = global.Promise;
+mongoose.connect(DB_CONNECTION_STRING);
+mongoose.connection.on("error", (err: Error) => console.error(err));

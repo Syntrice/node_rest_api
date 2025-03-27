0fd3c6516524a9a85bb2d528ba9a245919771e3e
Syntrice <syntrice@protonmail.com>
2025-03-27 12:44:46 +0000

    feat: Implement login endpoint controller

--- src/authentication/index.ts
+++ src/authentication/index.ts
@@ -1,11 +1,22 @@
 import crypto from 'crypto';
 
-const SALT_SIZE = 128;
+const SALT_SIZE = 32;
 const HASH_SIZE = 64;
-const ITERATIONS = 10000;
+const ITERATIONS = 100000;
+const SESSION_TOKEN_SIZE = 32;
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 export const generateSalt = () => crypto.randomBytes(SALT_SIZE).toString('base64');
 
 export const hashPassword = (salt: string, password: string) => {
     return crypto.pbkdf2Sync(password, salt, ITERATIONS, HASH_SIZE, 'sha512').toString('hex')
 }
+
+export const generateSessionToken = () => {
+    return crypto.randomBytes(SESSION_TOKEN_SIZE).toString('hex');
+}
+
+export const validatePassword = (password: string, salt: string, hash: string) => {
+    return hashPassword(salt, password) === hash;
+} 
--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,4 +1,9 @@
-import { generateSalt, hashPassword } from "authentication";
+import {
+    generateSalt,
+    generateSessionToken,
+    hashPassword,
+    validatePassword,
+} from "authentication";
 import { createUser, getUserByEmail, getUserByUsername } from "database/users";
 import express from "express";
 
@@ -25,7 +30,7 @@ export const register = async (req: express.Request, res: express.Response) => {
             res.status(409).send("Username already in use");
             return;
         }
-        
+
         // Hash password
         const salt = generateSalt();
         const hashedPassword = hashPassword(salt, password);
@@ -40,20 +45,59 @@ export const register = async (req: express.Request, res: express.Response) => {
             },
         });
         res.status(200).json(user).send();
-        return 
-
     } catch (error) {
         console.log(error);
-        res.status(500);
+        res.status(500).send();
     }
-}
+};
 
 export const login = async (req: express.Request, res: express.Response) => {
-  try {
-      
-  }
-  catch (error) {
-      console.log(error);
-      res.status(500);
-  }
-}
+    try {
+        const { email, password } = req.body;
+
+        // Validate request contains required fields
+        if (!email || !password) {
+            res.status(400).send("Missing required fields");
+            return;
+        }
+
+        // Get user from database, with password and salt
+        const user = await getUserByEmail(email).select(
+            "+authentication.salt +authentication.password"
+        );
+
+        // Check if user exists
+        if (!user) {
+            res.status(400).send("Email does not exist");
+            return;
+        }
+
+        // Validate password
+        if (
+            !validatePassword(
+                password,
+                user.authentication.salt,
+                user.authentication.password
+            )
+        ) {
+            res.status(401).send("Invalid password");
+            return;
+        }
+
+        // Create / update session token for user
+        user.authentication.sessionToken = generateSessionToken();
+
+        await user.save();
+
+        res.cookie("sessionToken", user.authentication.sessionToken, {
+            httpOnly: true,
+        })
+            .status(200)
+            .json(user)
+            .send();
+            
+    } catch (error) {
+        console.log(error);
+        res.status(500).send();
+    }
+};

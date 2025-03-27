416227fdd012123b6a037ff0018c99a0c776e0f9
Syntrice <syntrice@protonmail.com>
2025-03-27 13:06:44 +0000

    feat: Implement users controller and router
    Implement users controller and router to get all users

---
+++ src/controllers/users.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { getUsers } from "database/users";
+
+export const getAllUsers = async (
+    req: express.Request,
+    res: express.Response
+) => {
+    try {
+        const users = await getUsers();
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send();
+    }
+};
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import authentication from './authentication';
+import users from './users';
 
 const router = express.Router();
 
 export default (): express.Router => {
     authentication(router); // Add the authentication routes to the router
+    users(router) // Add the users routes to the router
     return router;
 };
